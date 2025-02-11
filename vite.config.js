ad528893c60481b6e6a416cac0402445decedcc3
Pankaj <pankajrushclick@gmail.com>
2025-02-11 15:19:01 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
 })
+
+// export default {
+//   base: '/myapp/',  // Adjust this to the subdirectory where the app is hosted
+// }
